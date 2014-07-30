a2bff9624fee75d77bd3167481f2466d11954fa0
Abrahan Cruz <starbrahan3@gmail.com>
2014-07-30 13:56:52 -0500

    Added encryption and decryption.

--- src/com/nyul/secretmessage/Cipher.java
+++ src/com/nyul/secretmessage/Cipher.java
@@ -3,13 +3,25 @@ package com.nyul.secretmessage;
 import java.util.Locale;
 
 public class Cipher {
-
+	
+	
 	public static String encrypt(String message) {
-		return message.toUpperCase(Locale.US);
+		char[] a = message.toCharArray();
+
+		for(int i=0; i < a.length;  i++){
+			a[i] = (char)( a[i] + 1);
+		}
+		return new String(a);	
 	}
 
 	public static String decrypt(String message) {
-		return message.toLowerCase(Locale.US);
+		char[] a = message.toCharArray();
+
+		for(int i=0; i < a.length;  i++){
+			a[i] = (char)( a[i] - 1);
+		}
+		return new String(a);	
 	}
 
+	
 }
